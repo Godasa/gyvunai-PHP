b26eb50c1089bea4a4f2f5ea15ac5db507659ac0
Godasa <40291931+Godasa@users.noreply.github.com>
2018-11-05 13:25:59 +0200

    commit

---
+++ public_html/model/Animal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Finkona
+ * Date: 11/5/2018
+ * Time: 11:40 AM
+ */
+
+class Animal
+{
+    public $type;
+    public $name;
+    public $weight;
+    public $height;
+    public $action;
+
+
+}
+
---
+++ public_html/model/Dog.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Finkona
+ * Date: 11/5/2018
+ * Time: 11:41 AM
+ */
+
+
+include_once __DIR__."Animal.php";
+
+class Dog extends Animal
+{
+
+}
